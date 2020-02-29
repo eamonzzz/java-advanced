80ad787b1a086ced6c774b911a6de01b927ad2e5
eamonzzz <eamon.zhang@outlook.com>
2020-02-29 17:17:43 +0800

    feat: modify 修改容器式单例
    - modify 修改容器式单例代码，添加一层判断，提高性能

--- 01.DesignPatterns/design-patterns/src/main/java/com/eamon/javadesignpatterns/singleton/container/ContainerSingleton.java
+++ 01.DesignPatterns/design-patterns/src/main/java/com/eamon/javadesignpatterns/singleton/container/ContainerSingleton.java
@@ -14,6 +14,11 @@ public class ContainerSingleton {
     private static Map<String, Object> ioc = new ConcurrentHashMap<String, Object>();
 
     public static Object getInstance(String className) {
+        // 加一层判断，如果已经存在了，就取出返回
+        if (ioc.containsKey(className)) {
+            return ioc.get(className);
+        }
+        // 否则，将ioc锁住，进行实例创建
         synchronized (ioc) {
             if (!ioc.containsKey(className)) {
                 Object object = null;
